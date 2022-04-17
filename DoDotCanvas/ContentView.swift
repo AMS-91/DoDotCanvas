8a16c0a0820a2cb8c60cbea3992ef412457a89aa
DEVready <amsmutu@gmail.com>
2022-04-17 21:48:10 +0900

    Initial Commit

---
+++ DoDotCanvas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoDotCanvas
+//
+//  Created by 안민수 on 2022/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoDotCanvas/DoDotCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoDotCanvasApp.swift
+//  DoDotCanvas
+//
+//  Created by 안민수 on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct DoDotCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
